39ceb0b7c9bb332d5ab9718f579916f4e8286b12
Alex Tun <aungbotun22@gmail.com>
2023-05-24 20:17:17 -0400

    next version

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {}
 module.exports = nextConfig
 module.exports = {
     distDir: 'build',
-    images: {
+    experimental: {
+      images: {
       unoptimized: true
     }
+  }
   };
