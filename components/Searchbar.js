e9f68de7aff26b102ff0a87f9bf47df1cbd5757d
ayemon <ayemonkyaw.amk@gmail.com>
2023-06-07 18:46:33 +0630

    color-contrast & sizes

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -34,7 +34,7 @@ export default function Searchbar() {
 
         />
         <button type="submit" scroll="false"
-          className="text-white right-2.5 bottom-2.5 bg-[#3e8193] focus:outline-none font-medium rounded-lg text-sm px-4 py-2">
+          className="text-white right-2.5 bottom-2.5 bg-[#2C6068] focus:outline-none font-medium rounded-lg text-sm px-4 py-2">
           ရှာပါ</button>
       </form>
       
