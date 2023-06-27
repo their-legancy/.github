d13cde0fc7b36416382afc07d6def577e1d523eb
ayemon <ayemonkyaw.amk@gmail.com>
2023-06-28 03:05:20 +0630

    connect with api hero id

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,50 @@
+import axios from 'axios';
 
-import fs from 'fs';
+// import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import Post from '../components/Post';
 import Banner from "../components/Banner";
-import search from "../search.json";
+// import search from "../search.json";
 import Head from 'next/head';
 import { sortByDate, slugify, ImageUrl } from '../utils';
 import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
 
-export default function Home() {
+export const getStaticProps = async () => {
+
+  const res = await axios.get(
+    // process.env.BACKEND_API+'api/hero'
+    process.env.BACKEND_API+'api/hero'
+  );
+
+  return {
+    props: {
+      searchPost: res.data,
+    },
+  };
+
+};
+export default function Home({ searchPost }) {
+
   const { query } = useRouter()
   const TempPosts = []
 
-  search.map(
+  searchPost.map(
     (post) => {
-      if (post.frontmatter.draft === false) {
-        if (post.frontmatter.name.toLowerCase().includes(query.q)) {
-          TempPosts.push(post)
-        } else {
-          TempPosts.push(null)
-        }
+
+      // if (post.draft === false) {
+      if (post.name.toLowerCase().includes(query.q)) {
+        TempPosts.push(post)
+      } else {
+        TempPosts.push(null)
       }
+      // }
     }
   )
 
-  //   remove null in posts 
+
+  // remove null in posts 
   const posts = TempPosts.filter(
     path => {
       return path && path
@@ -43,7 +61,6 @@ export default function Home() {
 
       <section id="search-result" className="w-full font-medium px-6 py-6">
         <div className="search-result-inner flex flex-wrap items-self-start justify-center">
-
 
 
           {
@@ -64,59 +81,6 @@ export default function Home() {
 
 
   )
-}
-
-export async function getStaticProps() {
-  // Get files from the posts dir
-  const files = fs.readdirSync(path.join('posts'));
-
-  // Get slug and frontmatter from posts
-  const tempPosts = files.map((filename) => {
-    // Create slug
-    const slug = filename.replace('.md', '');
-
-    // Get frontmatter
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8'
-    )
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-
-    if (frontmatter.draft === false) {
-      return {
-        slug,
-        frontmatter,
-      }
-    } else {
-      return null
-    }
-
-  })
-
-  //  remove null in tempPosts 
-  const posts = tempPosts.filter(
-    post => {
-      return post && post
-    }
-  )
-  const jsonString = JSON.stringify(posts)
-  fs.writeFileSync('./search.json', jsonString, err => {
-    if (err) {
-      console.log('Error writing file', err);
-    } else {
-      console.log('Successfully wrote file');
-    }
-  })
-
-  return {
-    props: {
-      posts: posts.sort(sortByDate),
-    },
-  }
-
-
 }
 
 
